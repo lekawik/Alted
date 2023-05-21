c70a86a3e5d3acbc25c706dff55c9423b419abf4
Soren SAMAMA <soren.samama@trisky.com>
2023-05-21 11:23:53 +0200

    Initial Commit

---
+++ Alted/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alted
+//
+//  Created by Soren SAMAMA on 21/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
